d8201ffcd1c82189a7c5a25f18e0ac8acc03a4a2
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2023-12-28 13:38:44 +1300

    heal function & surrender function done

--- app.js
+++ app.js
@@ -8,28 +8,52 @@ Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             counter: 0,
+            winner: null
         }
     },
 
     computed:{
       monsterBarStyles(){
-            if(this.monsterHealth < 0){
-                return {width: '0%'}
-            }
+        if (this.monsterHealth < 0){
+            return {width: '0%'}
+        }
             return {width: this.monsterHealth + '%'}
       },
         playerBarStyles(){
-                if(this.playerHealth < 0){
-                    return {width: '0%'}
-                }
+            if (this.playerHealth < 0){
+                return {width: '0%'}
+            }
                 return {width: this.playerHealth + '%'}
         },
         mayUseSpecialAttack(){
             return this.counter % 3 !== 0;
         }
     },
+    watch:{
+        playerHealth(value){
+            if(value <= 0 && this.monsterHealth <= 0){
+                this.winner = 'draw'
+            }else if(value <= 0){
+                this.winner = 'monster'
+            }
+        },
+        monsterHealth(value){
+            if(value <= 0 && this.playerHealth <= 0){
+                this.winner = 'draw'
+            }else if(value <= 0){
+                this.winner = 'player'
+            }
+        }
+    
+    },
 
     methods: {
+        startGame(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.counter = 0;
+        },
         attackMonster(){
             const attackValue = getRandomNumber(5, 12);
             this.monsterHealth -= attackValue;
@@ -46,6 +70,19 @@ Vue.createApp({
             this.attackPlayer();
             this.counter++;
         },
+        heal(){
+            const healValue = getRandomNumber(8,20);
+            if(this.playerHealth + healValue > 100){
+                this.playerHealth = 100;
+            }else{
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+            this.counter++;
+        },
+        surrender(){
+            this.winner = 'monster';
+        }
 
     }
 }).mount("#game")
